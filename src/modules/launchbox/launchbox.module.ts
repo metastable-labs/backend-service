d44b014a73fb83674e07d9a7b4cb4853db39ceb7
iamnotstatic <staticdev20046@gmail.com>
2024-07-04 01:32:34 +0700

    privy authentication

--- src/modules/launchbox/launchbox.module.ts
+++ src/modules/launchbox/launchbox.module.ts
@@ -6,13 +6,16 @@ import {
   LaunchboxToken,
   LaunchboxTokenHolder,
   LaunchboxTokenTransaction,
+  LaunchboxUser,
 } from './entities/launchbox.entity';
 import { CloudinaryModule } from '../../common/helpers/cloudinary/cloudinary.module';
 import { FarcasterModule } from '../../common/helpers/farcaster/farcaster.module';
 import { ContractModule } from '../../common/helpers/contract/contract.module';
 import { SharedModule } from '../../common/helpers/shared/shared.module';
-import { AnalyticModule } from 'src/common/helpers/analytic/analytic.module';
+import { AnalyticModule } from '../../common/helpers/analytic/analytic.module';
 import { env } from '../../common/config/env';
+import { PrivyModule } from '../../common/helpers/privy/privy.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
@@ -20,12 +23,18 @@ import { env } from '../../common/config/env';
       LaunchboxToken,
       LaunchboxTokenHolder,
       LaunchboxTokenTransaction,
+      LaunchboxUser,
     ]),
     CloudinaryModule,
     FarcasterModule,
     ContractModule,
     SharedModule,
     AnalyticModule,
+    PrivyModule,
+    JwtModule.register({
+      secret: env.jwt.secret,
+      signOptions: { expiresIn: env.jwt.expiresIn },
+    }),
   ],
   providers: [LaunchboxService],
   controllers: [LaunchboxController],
