0e8384a717c75c100fb27e01f6d0d19d0a9ac0dc
iamnotstatic <staticdev20046@gmail.com>
2024-07-04 01:31:17 +0700

    Added privy util service

---
+++ src/common/helpers/privy/privy.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { PrivyClient, User } from '@privy-io/server-auth';
+import { env } from '../../config/env';
+
+@Injectable()
+export class PrivyService {
+  public readonly clinet: PrivyClient;
+  constructor() {
+    this.clinet = new PrivyClient(env.privy.appId, env.privy.appSecret);
+  }
+  
+}
