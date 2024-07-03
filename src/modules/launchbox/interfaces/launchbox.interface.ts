97d285b26a09060656d175cba22cacb42742168c
iamnotstatic <staticdev20046@gmail.com>
2024-07-03 08:58:12 +0700

    Add interfaces for Appearance, Navigation, HeroSection, Tokenomics, Faq, and Footer

--- src/modules/launchbox/interfaces/launchbox.interface.ts
+++ src/modules/launchbox/interfaces/launchbox.interface.ts
@@ -15,3 +15,50 @@ export interface Social {
     };
   };
 }
+
+interface Appearance {
+  primary_color: string;
+  secondary_color: string;
+}
+
+export interface Navigation {
+  logo_url: string;
+  buy_button_link: string;
+}
+
+interface HeroSection {
+  title: string;
+  description: string;
+  image_url: string;
+}
+
+interface Tokenomics {
+  [key: string]: string;
+}
+
+interface FaqQuestion {
+  title: string;
+  answer: string;
+}
+
+interface Faq {
+  title: string;
+  description: string;
+  question: FaqQuestion[];
+}
+
+interface Footer {
+  x: string;
+  farcaster: string;
+  telegram: string;
+  chain_explorer: string;
+}
+
+export interface WebsiteBuilder {
+  appearance: Appearance;
+  navigation: Navigation;
+  hero_section: HeroSection;
+  tokenomics: Tokenomics;
+  faq: Faq;
+  footer: Footer;
+}
