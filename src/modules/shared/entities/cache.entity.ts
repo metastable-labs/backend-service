5c08671958730cf927a3ab3219434f4ead7c1464
iamnotstatic <staticdev20046@gmail.com>
2024-08-19 12:55:41 +0400

    introduced a cache layer

---
+++ src/modules/shared/entities/cache.entity.ts
@@ -0,0 +1,25 @@
+import { Column, CreateDateColumn, Entity, ObjectIdColumn, PrimaryColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('shared_cache')
+export class SharedCache {
+  @ObjectIdColumn()
+  _id: string;
+
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  key: string;
+
+  @Column({ type: 'json' })
+  value: any;
+
+  @Column()
+  expires: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
